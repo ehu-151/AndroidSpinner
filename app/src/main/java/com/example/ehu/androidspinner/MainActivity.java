7f07d7e00830446db3ead67a9a1a7dce0efc9e95
ehu <fehuehuf@gmail.com>
2017-06-04 22:58:45 +0900

    フリック後アニメーション開始機能実装
    フリックするとアニメーションが開始
    長押し又はアプリを閉じるとアニメーションが停止

--- app/src/main/java/com/example/ehu/androidspinner/MainActivity.java
+++ app/src/main/java/com/example/ehu/androidspinner/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.ehu.androidspinner;
 
+import android.annotation.SuppressLint;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
     //ハンドスピナー画像の宣言
@@ -21,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
     // タッチイベントを処理するためのインタフェース
     private GestureDetector mGestureDetector;
-    private double spinnerVelocity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,25 @@ public class MainActivity extends AppCompatActivity {
         //idと関連付け
         handSpinnerImage = (ImageView) findViewById(R.id.handSpinner);
 
+        //ジェスチャーのインスタンス化
         mGestureDetector = new GestureDetector(this, mOnGestureListener);
+
+        //バイブレーションの初期化
+        vb = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopSpinner();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopSpinner();
     }
 
     // タッチイベント
@@ -50,45 +74,53 @@ public class MainActivity extends AppCompatActivity {
                 // スピードを出力
                 float velocity_y = Math.abs(velocityY);
                 Log.d("speed", " 縦の移動スピード:" + velocity_y);
+                startSpinner(velocity_y);
                 return true;
             } catch (Exception e) {
                 Log.e("Exception", e.toString());
                 return false;
             }
         }
+
+        @Override
+        public void onLongPress(MotionEvent event) {
+            stopSpinner();
+        }
     };
 
-    //ハンドスピナーの速さ設定
-    public void setSpinnerVelocity(double volocity) {
-        spinnerVelocity = volocity;
-    }
-
-    //ハンドスピナーの速さ取得
-    public double getSpinnerVolocity() {
-        return spinnerVelocity;
-    }
 
     //ハンドスピナーを回転させる
-
-    public void startSpinner(View v) {
+    public void startSpinner(double velocity) {
 
         RotateAnimation rotate = new RotateAnimation(0, 360, handSpinnerImage.getWidth() / 2, (handSpinnerImage.getHeight() / 2) + 75); // 画像の中心を軸に、0度から360度にかけて回転
-        rotate.setDuration(rotateTime); // 3000msかけてアニメーションする
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        double xM = (dm.widthPixels / dm.xdpi * 2.54) * 0.01;
+        double yM = (dm.heightPixels / dm.ydpi * 2.54) * 0.01;
+
+        Log.d("MainActivity", "幅 = " + xM + "m" + "   高さ = " + yM + "m");
+        double m_s = velocity * yM / dm.heightPixels;
+        Log.d("m/s", String.valueOf(m_s));
+
+        double T = 2 * 3.14 * yM / m_s;
+        rotate.setDuration((long) (T * 1000)); // 3000msかけてアニメーションする
+        Log.d("周期", String.valueOf(T));
         rotate.setInterpolator(new LinearInterpolator());   //繰り返し終了時止まらない
-        rotate.setRepeatCount(repeatCount - 1); //ずっと繰り返す
+        rotate.setRepeatCount(rotate.INFINITE); //ずっと繰り返す
         handSpinnerImage.startAnimation(rotate); // アニメーション適用
 
-        //バイブレーションで振動させる
-        vb = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        vb.vibrate(rotateTime * repeatCount);
+        // バイブレーションで振動させる
+//        vb.vibrate(rotateTime * repeatCount);
 
         Log.d("start", "回転");
     }
 
     //ハンドスピナーを止める
-    public void stopSpinner(View v) {
+    public void stopSpinner() {
         handSpinnerImage.clearAnimation();  //回転を止める
-        vb.cancel();    //バイブレーションを止める
+//        vb.cancel();    //バイブレーションを止める
 
         Log.d("stop", "停止");
     }
